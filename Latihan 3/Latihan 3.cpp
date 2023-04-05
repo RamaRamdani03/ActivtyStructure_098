bc63733f3850d88e36e875be822b4b373e5447b4
RamaRamdani03 <ramaramdani449@gamil.com>
2023-04-05 11:40:27 +0700

    menambahkan alamat desa dan kota

--- Latihan 3/Latihan 3.cpp
+++ Latihan 3/Latihan 3.cpp
@@ -22,5 +22,23 @@ int main() {
 		cout << "Masukan Nim : ";
 		cin.getline(mhs[i].nim, 12);
 		cout << "Masukan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "Masukan nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat : ";
+		cout << "\n\tMasuka Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tMasukan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		cout << "\n\nNIM : " << mhs[i].nim;
+		cout << "\nUmur : " << mhs[i].umur;
+		cout << "\nNama : " << mhs[i].nama;
+		cout << "\nAlamat :";
+		cout << "\n\tDesa : " << mhs[i].alamat.desa;
+		cout << "\n\tKota : " << mhs[i].alamat.kota;
 	}
 }
